580239fcfb6ee6c582c856e7b1de5c00390ff7d6
Thekolllol90 <neosvarre@hotmail.se>
2019-09-25 11:58:12 +0200

    begining

--- src/v39/läxa_flagga.java
+++ src/v39/läxa_flagga.java
@@ -1,4 +1,35 @@
 package v39;
 
-public class läxa_flagga {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class läxa_flagga extends Application {
+    public static void main(String[] args) { launch(args);}
+
+    public void start(Stage primaryStage) throws Exception{
+        String name = "";
+
+        Rectangle squ = new Rectangle();
+        Rectangle rec = new Rectangle();
+        Rectangle back = new Rectangle();
+
+        StackPane swe = new Group(back);
+        swe.getChildren(rec);
+        swe.getChildren(squ);
+
+        Scene scene = new Scene(swe, 600, 300);
+        primaryStage.setTitle(name);
+
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+    private static void flagga_1(){
+
+    }
 }
