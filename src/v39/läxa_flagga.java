d07be853b0857c8bf8876bef9be5273c3225b058
Thekolllol90 <neosvarre@hotmail.se>
2019-09-26 08:37:36 +0200

    norge

--- src/v39/läxa_flagga.java
+++ src/v39/läxa_flagga.java
@@ -13,31 +13,44 @@ public class läxa_flagga extends Application {
     public static void main(String[] args) { launch(args);}
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Rectangle right = new Rectangle();
-        Rectangle mid = new Rectangle();
+        Rectangle front_left = new Rectangle();
         Rectangle left = new Rectangle();
+        Rectangle front_mid = new Rectangle();
+        Rectangle mid = new Rectangle();
+        Rectangle back = new Rectangle();
 
-        right.setTranslateX(-200.0f);
-        right.setHeight(300.0f);
-        right.setWidth(200.0f);
-        right.setFill(Color.BLACK);
+        back.setHeight(300.0f);
+        back.setWidth(600.0f);
+        back.setFill(Color.RED);
 
-        mid.setTranslateX(200.0f);
-        mid.setHeight(300.0f);
-        mid.setWidth(200.0f);
-        mid.setFill(Color.YELLOW);
+        front_mid.setHeight(40.0f);
+        front_mid.setWidth(600.0f);
+        front_mid.setFill(Color.BLUE);
 
-        left.setTranslateX(400.0f);
+        mid.setHeight(50.0f);
+        mid.setWidth(600.0f);
+        mid.setFill(Color.WHITE);
+
+
+        front_left.setTranslateX(-100.0f);
+        front_left.setHeight(300.0f);
+        front_left.setWidth(40.0f);
+        front_left.setFill(Color.BLUE);
+
+        left.setTranslateX(-100.0f);
         left.setHeight(300.0f);
-        left.setWidth(200.0f);
-        left.setFill(Color.RED);
+        left.setWidth(50.0f);
+        left.setFill(Color.WHITE);
 
-        BorderPane fra = new BorderPane(right);
-        fra.getChildren().add(mid);
-        fra.getChildren().add(left);
+        StackPane swe = new StackPane(back);
+        swe.getChildren().add(left);
+        swe.getChildren().add(mid);
+        swe.getChildren().add(front_left);
+        swe.getChildren().add(front_mid);
 
-        Scene scene = new Scene(fra, 600, 300);
-        primaryStage.setTitle("flaggor");
+
+        Scene scene = new Scene(swe, 600, 300);
+        primaryStage.setTitle("flagga 1");
 
         primaryStage.setScene(scene);
 
@@ -103,4 +116,48 @@ public class läxa_flagga extends Application {
 
         primaryStage.show();
     }
-}
+    private static void flagga_3(Stage primaryStage) throws  Exception{
+        Rectangle front_left = new Rectangle();
+        Rectangle left = new Rectangle();
+        Rectangle front_mid = new Rectangle();
+        Rectangle mid = new Rectangle();
+        Rectangle back = new Rectangle();
+
+        back.setHeight(300.0f);
+        back.setWidth(600.0f);
+        back.setFill(Color.RED);
+
+        front_mid.setHeight(40.0f);
+        front_mid.setWidth(600.0f);
+        front_mid.setFill(Color.BLUE);
+
+        mid.setHeight(50.0f);
+        mid.setWidth(600.0f);
+        mid.setFill(Color.WHITE);
+
+
+        front_left.setTranslateX(-100.0f);
+        front_left.setHeight(300.0f);
+        front_left.setWidth(40.0f);
+        front_left.setFill(Color.BLUE);
+
+        left.setTranslateX(-100.0f);
+        left.setHeight(300.0f);
+        left.setWidth(50.0f);
+        left.setFill(Color.WHITE);
+
+        StackPane swe = new StackPane(back);
+        swe.getChildren().add(left);
+        swe.getChildren().add(mid);
+        swe.getChildren().add(front_left);
+        swe.getChildren().add(front_mid);
+
+
+        Scene scene = new Scene(swe, 600, 300);
+        primaryStage.setTitle("flagga 1");
+
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+ }
