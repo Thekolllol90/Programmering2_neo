19d117b392f810c6430c6ee155cead5c2e781ecf
Thekolllol90 <neosvarre@hotmail.se>
2019-09-28 13:16:38 +0200

    försöker_göra_knappar

--- src/v39/läxa_flagga.java
+++ src/v39/läxa_flagga.java
@@ -1,45 +1,51 @@
 package v39;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.scene.text.Text;
 
 public class läxa_flagga extends Application {
     public static void main(String[] args) { launch(args);}
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Rectangle top = new Rectangle();
-        Rectangle mid = new Rectangle();
-        Rectangle bot = new Rectangle();
+        int flag = 0;
+        Button btn = new Button();
+        btn.setText("Sweden");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                int flag = 1;
+            }
+        });
+        System.out.println(flag);
 
-        top.setHeight(100.0f);
-        top.setWidth(600.0f);
-        top.setFill(Color.RED);
 
-        mid.setHeight(100.0f);
-        mid.setWidth(600.0f);
-        mid.setFill(Color.WHITE);
 
-        bot.setHeight(100.0f);
-        bot.setWidth(600.0f);
-        bot.setFill(Color.BLUE);
+    }
+    private static int pick(int flag,  Stage primaryStage) throws Exception{
+        int res = 0;
+        Text text = new Text("Välj en flagga!");
+        text.setX(50);
+        text.setY(50);
 
-        BorderPane fra = new BorderPane();
-        fra.setTop(top);
-        fra.setCenter(mid);
-        fra.setBottom(bot);
 
-        Scene scene = new Scene(fra, 600, 300);
+
+        Group group = new Group();
+        Scene scene = new Scene(group, 600, 300);
+
         primaryStage.setTitle("flaggor");
-
         primaryStage.setScene(scene);
-
         primaryStage.show();
+
+        return res;
     }
     private static void flagga_1(Stage primaryStage) throws Exception{
         Rectangle squ = new Rectangle();
@@ -75,26 +81,24 @@ public class läxa_flagga extends Application {
         Rectangle mid = new Rectangle();
         Rectangle left = new Rectangle();
 
-        right.setTranslateX(-200.0f);
         right.setHeight(300.0f);
         right.setWidth(200.0f);
         right.setFill(Color.BLACK);
 
-        mid.setTranslateX(200.0f);
         mid.setHeight(300.0f);
         mid.setWidth(200.0f);
         mid.setFill(Color.YELLOW);
 
-        left.setTranslateX(400.0f);
         left.setHeight(300.0f);
         left.setWidth(200.0f);
         left.setFill(Color.RED);
 
-        BorderPane fra = new BorderPane(right);
-        fra.getChildren().add(mid);
-        fra.getChildren().add(left);
+        BorderPane tys = new BorderPane();
+        tys.setRight(left);
+        tys.setCenter(mid);
+        tys.setLeft(right);
 
-        Scene scene = new Scene(fra, 600, 300);
+        Scene scene = new Scene(tys, 600, 300);
         primaryStage.setTitle("flaggor");
 
         primaryStage.setScene(scene);
@@ -131,14 +135,14 @@ public class läxa_flagga extends Application {
         left.setWidth(50.0f);
         left.setFill(Color.WHITE);
 
-        StackPane swe = new StackPane(back);
-        swe.getChildren().add(left);
-        swe.getChildren().add(mid);
-        swe.getChildren().add(front_left);
-        swe.getChildren().add(front_mid);
+        StackPane nor = new StackPane(back);
+        nor.getChildren().add(left);
+        nor.getChildren().add(mid);
+        nor.getChildren().add(front_left);
+        nor.getChildren().add(front_mid);
 
 
-        Scene scene = new Scene(swe, 600, 300);
+        Scene scene = new Scene(nor, 600, 300);
         primaryStage.setTitle("flaggor");
 
         primaryStage.setScene(scene);
@@ -146,6 +150,32 @@ public class läxa_flagga extends Application {
         primaryStage.show();
     }
     private static void flagga_4(Stage primaryStage) throws Exception{
+        Rectangle top = new Rectangle();
+        Rectangle mid = new Rectangle();
+        Rectangle bot = new Rectangle();
 
+        top.setHeight(100.0f);
+        top.setWidth(600.0f);
+        top.setFill(Color.RED);
+
+        mid.setHeight(100.0f);
+        mid.setWidth(600.0f);
+        mid.setFill(Color.WHITE);
+
+        bot.setHeight(100.0f);
+        bot.setWidth(600.0f);
+        bot.setFill(Color.BLUE);
+
+        BorderPane fra = new BorderPane();
+        fra.setTop(top);
+        fra.setCenter(mid);
+        fra.setBottom(bot);
+
+        Scene scene = new Scene(fra, 600, 300);
+        primaryStage.setTitle("flaggor");
+
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
     }
  }
