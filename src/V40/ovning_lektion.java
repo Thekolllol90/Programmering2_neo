10af3087585a3a94902d28874f066a97e194878e
Thekolllol90 <neosvarre@hotmail.se>
2019-10-10 13:09:41 +0200

    print buttons
    nya knappar som skrivs ut

--- src/V40/ovning_lektion.java
+++ src/V40/ovning_lektion.java
@@ -3,12 +3,15 @@ package V40;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.shape.Rectangle;
-import java.awt.*;
 
 
 public class ovning_lektion extends Application {
@@ -18,8 +21,26 @@ public class ovning_lektion extends Application {
     public void start(Stage primaryStage) throws Exception{
         TextField input = new TextField();
         TextField output = new TextField();
-        
-        Button btn0 = new Button();
+
+        String[][] buttons = {
+            { "1", "2", "3", "+" },
+            { "4", "5", "6", "-" },
+            { "7", "8", "9", "*"},
+            { "C", "0", "=", "/"}
+        };
+
+        GridPane grid = new GridPane();
+
+        for(int y = 0; y < buttons.length; y++) {
+            for(int x = 0; x < buttons[y].length; x++) {
+                String name = buttons[y][x];
+                Button button = new Button(name);
+
+                grid.add(button, x, y);
+            }
+        }
+
+        /*Button btn0 = new Button();
         Button btn1 = new Button();
         Button btn2 = new Button();
         Button btn3 = new Button();
@@ -34,6 +55,9 @@ public class ovning_lektion extends Application {
         Button btnMulti = new Button();
         Button btnDiv = new Button();
         Button btnKom = new Button();
+        Button btnCalc = new Button();*/
+
+        //btn0.getWidth();
 
         Rectangle rec = new Rectangle();
 
@@ -41,7 +65,7 @@ public class ovning_lektion extends Application {
         rec.setWidth(600.0f);
         rec.setFill(Color.BLACK);
 
-        Group test = new Group(rec);
+        Group test = new Group(grid);
         Scene scene = new Scene(test, 600, 600);
         primaryStage.setTitle("Test");
         primaryStage.setScene(scene);
