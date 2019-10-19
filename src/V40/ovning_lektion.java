0b60a78fc08c88327b74d452121a90995f32604b
Thekolllol90 <neosvarre@hotmail.se>
2019-10-19 15:18:02 +0200

    börjar om
    patrik hjälpte mig med denna verison och fattar inte jätte mycket av de och hur jag ska vidare utvekla så jag börjar om och gör allt själv

--- src/V40/ovning_lektion.java
+++ src/V40/ovning_lektion.java
@@ -20,55 +20,7 @@ public class ovning_lektion extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Label input = new Label();
 
-        String[][] buttons = {
-            { "1", "2", "3", "+" },
-            { "4", "5", "6", "-" },
-            { "7", "8", "9", "*"},
-            { "C", "0", "=", "/"}
-        };
-
-        GridPane grid = new GridPane();
-
-        final ColumnConstraints col = new ColumnConstraints(100, Control.USE_COMPUTED_SIZE, Control.USE_COMPUTED_SIZE);
-        col.setHgrow(Priority.ALWAYS);
-        grid.getColumnConstraints().addAll(col, col, col, col);
-
-
-        grid.add(input, 0, 0, 4, 1);
-
-        for(int y = 0; y < buttons.length; y++) {
-            for(int x = 0; x < buttons[y].length; x++) {
-                String name = buttons[y][x];
-                Button button = new Button(name);
-                button.setMinWidth(100);
-                button.setOnAction(event -> {
-                    if(name.equals("C"))
-                        return;
-                    String text = input.getText();
-                    StringBuilder builder = new StringBuilder();
-                    builder.append(text);
-                    builder.append(name);
-                    input.setText(builder.toString());
-                });
-
-                grid.add(button, x, y + 1);
-            }
-        }
-
-        Rectangle rec = new Rectangle();
-
-        rec.setHeight(600.0f);
-        rec.setWidth(600.0f);
-        rec.setFill(Color.BLACK);
-
-        Group test = new Group(grid);
-        Scene scene = new Scene(test, 600, 600);
-        primaryStage.setTitle("Test");
-        primaryStage.setScene(scene);
-        primaryStage.show();
 
     }
-
 }
