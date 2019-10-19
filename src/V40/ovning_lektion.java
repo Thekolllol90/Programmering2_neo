499064ed570b1be76cf2872a6e5035955239b8ca
Thekolllol90 <neosvarre@hotmail.se>
2019-10-19 15:24:04 +0200

    skappat alla knappar jag behöver

--- src/V40/ovning_lektion.java
+++ src/V40/ovning_lektion.java
@@ -14,7 +14,7 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.shape.Rectangle;
 
-
+//gör så när man klickar på exempel + så loppar den igenom så de inte blir ++ i rutan
 public class ovning_lektion extends Application {
     public static void main(String[] args) { launch(args); }
 
@@ -23,4 +23,29 @@ public class ovning_lektion extends Application {
 
 
     }
+    public static void layout(Stage primaryStage) throws Exception{
+        Button btn0 = new Button();
+        Button btn1 = new Button();
+        Button btn2 = new Button();
+        Button btn3 = new Button();
+        Button btn4 = new Button();
+        Button btn5 = new Button();
+        Button btn6 = new Button();
+        Button btn7 = new Button();
+        Button btn8 = new Button();
+        Button btn9 = new Button();
+        Button btnPlus = new Button();
+        Button btnMinus = new Button();
+        Button btnDiv = new Button();
+        Button btnTimes = new Button();
+        Button btnMod = new Button();
+        Button btnClear = new Button();
+        Button btnEq = new Button();
+
+
+
+
+    }
+
+
 }
