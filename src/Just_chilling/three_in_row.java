e8e60c97300248f2864113e3ce0616a03f489c4a
Thekolllol90 <neosvarre@hotmail.se>
2020-01-30 19:37:54 +0100

    some new functions and changes

--- src/Just_chilling/three_in_row.java
+++ src/Just_chilling/three_in_row.java
@@ -24,12 +24,13 @@ public class three_in_row extends Application {
         Button sqr5 = new Button();
         Button sqr6 = new Button();
         Button sqr7 = new Button();
-        Button sqr9 = new Button();
         Button sqr8 = new Button();
+        Button sqr9 = new Button();
+        Button reset = new Button();
 
 
 
-        Button[] squares = {sqr1, sqr2, sqr3, sqr4, sqr5, sqr6, sqr7, sqr8, sqr9};
+        Button[] squares = {sqr1, sqr2, sqr3, sqr4, sqr5, sqr6, sqr7, sqr8, sqr9, reset};
         return squares;
     }
 
@@ -51,13 +52,18 @@ public class three_in_row extends Application {
                             turn[0] = 1;
                             pressed[finalCurrent] = true;
                             color[finalCurrent] = 'R';
-                            checkRow(funcsqr, color, finalCurrent);
-                        }else if (turn[0] == 1 && pressed[finalCurrent] == false){
+                            checkRow(funcsqr, color);
+                            checkFull(funcsqr, color, pressed);
+                        }else if(turn[0] == 1 && pressed[finalCurrent] == false){
                             funcsqr[finalCurrent].setStyle("-fx-base: blue;");
                             turn[0] = 0;
                             pressed[finalCurrent] = true;
                             color[finalCurrent] = 'B';
-                            checkRow(funcsqr, color, finalCurrent);
+                            checkRow(funcsqr, color);
+                            checkFull(funcsqr, color, pressed);
+                        }
+                        if(finalCurrent == 9){
+                            reset(funcsqr, color, pressed);
                         }
                     }
                 });
@@ -66,91 +72,98 @@ public class three_in_row extends Application {
         return funcsqr;
     }
 
-    public static Button[] checkRow(Button[] funcsqr, char[] color, int finalCurrent){
+    public static Button[] reset(Button[] funcsqr, char[] color, Boolean[] pressed){
+        for(int i = 0; i < funcsqr.length; i++){
+            pressed[i] = false;
+            color[i] = ' ';
+            funcsqr[i].setStyle("-fx-base: white");
+        }
+
+        return funcsqr;
+    }
+
+    public static Button[] checkFull(Button[] funcsqr, char[] color, Boolean[] pressed){
+        int check = 0;
+        for(int i = 0; i < color.length; i++){
+            if(color[i] == ' '){
+                check++;
+            }
+        }
+        if(check == 0){
+            reset(funcsqr, color, pressed);
+        }
+
+
+        return funcsqr;
+    }
+
+    public static Button[] colorAllRed(Button[] funcsqr){
+        for(int i = 0; i < funcsqr.length; i++){
+            funcsqr[i].setStyle("-fx-base: red;");
+        }
+        return funcsqr;
+    }
+
+    public static Button[] colorAllBlue(Button[] funcsqr){
+        for(int i = 0; i < funcsqr.length; i++){
+            funcsqr[i].setStyle("-fx-base: blue;");
+        }
+        return funcsqr;
+    }
+
+    public static Button[] checkRow(Button[] funcsqr, char[] color){
         if(color[0] == 'R'){
             if(color[1] == 'R' && color[2] == 'R'){
-                for(int i = 0; i < funcsqr.length; i++){
-                    funcsqr[i].setStyle("-fx-base: red;");
-                }
+                colorAllRed(funcsqr);
             }else if(color[4] == 'R' && color[8] == 'R'){
-                for(int i = 0; i < funcsqr.length; i++){
-                    funcsqr[i].setStyle("-fx-base: red;");
-                }
+                colorAllRed(funcsqr);
             }else if(color[3] == 'R' && color[6] == 'R'){
-                for(int i = 0; i < funcsqr.length; i++){
-                    funcsqr[i].setStyle("-fx-base: red;");
-                }
+                colorAllRed(funcsqr);
             }
         }else if(color[1] == 'R'){
             if(color[4] == 'R' && color[7] == 'R'){
-                for(int i = 0; i < funcsqr.length; i++){
-                    funcsqr[i].setStyle("-fx-base: red;");
-                }
+                colorAllRed(funcsqr);
             }
         }else if(color[2] == 'R'){
             if(color[5] == 'R' && color[8] == 'R'){
-                for(int i = 0; i < funcsqr.length; i++){
-                    funcsqr[i].setStyle("-fx-base: red;");
-                }
+                colorAllRed(funcsqr);
             }
         }else if(color[3] == 'R'){
             if(color[4] == 'R' && color[5] == 'R'){
-                for(int i = 0; i < funcsqr.length; i++){
-                    funcsqr[i].setStyle("-fx-base: red;");
-                }
+                colorAllRed(funcsqr);
             }
         }else if(color[6] == 'R'){
             if(color[4] == 'R' && color[2] == 'R'){
-                for(int i = 0; i < funcsqr.length; i++){
-                    funcsqr[i].setStyle("-fx-base: red;");
-                }
+                colorAllRed(funcsqr);
             }else if(color[7] == 'R' && color[8] == 'R'){
-                for(int i = 0; i < funcsqr.length; i++){
-                    funcsqr[i].setStyle("-fx-base: red;");
-                }
+                colorAllRed(funcsqr);
             }
         }
         if(color[0] == 'B'){
             if(color[1] == 'B' && color[2] == 'B'){
-                for(int i = 0; i < funcsqr.length; i++){
-                    funcsqr[i].setStyle("-fx-base: blue;");
-                }
+                colorAllBlue(funcsqr);
             }else if(color[4] == 'B' && color[8] == 'B'){
-                for(int i = 0; i < funcsqr.length; i++){
-                    funcsqr[i].setStyle("-fx-base: blue;");
-                }
+                colorAllBlue(funcsqr);
             }else if(color[3] == 'B' && color[6] == 'B'){
-                for(int i = 0; i < funcsqr.length; i++){
-                    funcsqr[i].setStyle("-fx-base: blue;");
-                }
+                colorAllBlue(funcsqr);
             }
         }else if(color[1] == 'B'){
             if(color[4] == 'B' && color[7] == 'B'){
-                for(int i = 0; i < funcsqr.length; i++){
-                    funcsqr[i].setStyle("-fx-base: blue;");
-                }
+                colorAllBlue(funcsqr);
             }
         }else if(color[2] == 'B'){
             if(color[5] == 'B' && color[8] == 'B'){
-                for(int i = 0; i < funcsqr.length; i++){
-                    funcsqr[i].setStyle("-fx-base: blue;");
-                }
+                colorAllBlue(funcsqr);
             }
         }else if(color[3] == 'B'){
             if(color[4] == 'B' && color[5] == 'B'){
-                for(int i = 0; i < funcsqr.length; i++){
-                    funcsqr[i].setStyle("-fx-base: blue;");
-                }
+                colorAllBlue(funcsqr);
             }
         }else if(color[6] == 'B'){
             if(color[4] == 'B' && color[2] == 'B'){
-                for(int i = 0; i < funcsqr.length; i++){
-                    funcsqr[i].setStyle("-fx-base: blue;");
-                }
+                colorAllBlue(funcsqr);
             }else if(color[7] == 'B' && color[8] == 'B'){
-                for(int i = 0; i < funcsqr.length; i++){
-                    funcsqr[i].setStyle("-fx-base: blue;");
-                }
+                colorAllBlue(funcsqr);
             }
         }
 
@@ -166,7 +179,7 @@ public class three_in_row extends Application {
         StackPane view = new StackPane();
         view.getChildren().add(layout());
 
-        Scene scene = new Scene(view,300, 300);
+        Scene scene = new Scene(view,300, 350);
         primaryStage.setTitle("3 in row");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -178,6 +191,11 @@ public class three_in_row extends Application {
         for(int i = 0; i < squares.length; i++){
             squares[i].setMinWidth(100.0f);
             squares[i].setMinHeight(100.0f);
+            if(i == 9){
+                squares[i].setMaxWidth(300.0f);
+                squares[i].setMinHeight(50.0f);
+                squares[i].setText("Reset");
+            }
         }
 
         HBox row1 = new HBox();
@@ -190,7 +208,7 @@ public class three_in_row extends Application {
         row3.getChildren().addAll(squares[6], squares[7], squares[8]);
 
         VBox layout = new VBox();
-        layout.getChildren().addAll(row1,row2,row3);
+        layout.getChildren().addAll(squares[9],row1,row2,row3);
 
         return layout;
     }
