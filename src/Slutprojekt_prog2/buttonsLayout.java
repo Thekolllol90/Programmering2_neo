774558d1722c6a24ebc2656e21fc8eb4822bf2d1
Thekolllol90 <neosvarre@hotmail.se>
2020-04-29 12:04:22 +0200

    har gjort klart classen som gar knapparna storlek och lägger dom 3x3

--- src/Slutprojekt_prog2/buttonsLayout.java
+++ src/Slutprojekt_prog2/buttonsLayout.java
@@ -1,6 +1,7 @@
 package Slutprojekt_prog2;
 
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class buttonsLayout {
@@ -8,15 +9,27 @@ public class buttonsLayout {
 
     public static Button[] buttonStyle(Button[] buttons){
         for(int i = 0; i < buttons.length; i++){
-
+            buttons[i].setMinWidth(100.0f);
+            buttons[i].setMinHeight(100.0f);
         }
 
 
         return buttons;
     }
 
-    public static VBox layout(){
-        VBox x = new VBox();
-        return x;
+    public static VBox layout(Button[] buttons){
+        HBox row1 = new HBox();
+        row1.getChildren().addAll(buttons[0], buttons[1], buttons[2]);
+
+        HBox row2 = new HBox();
+        row1.getChildren().addAll(buttons[0], buttons[1], buttons[2]);
+
+        HBox row3 = new HBox();
+        row1.getChildren().addAll(buttons[0], buttons[1], buttons[2]);
+
+        VBox layout = new VBox();
+        layout.getChildren().addAll(row1, row2, row3);
+
+        return layout;
     }
 }
